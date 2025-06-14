f02d5f5ba58bef7f8c3e41f7c216d894b7038e6c
Zoheb Ansari <zoheb7532@gmail.com>
2025-06-15 00:12:38 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,11 +12,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://room-kp5a.onrender.com',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://room-kp5a.onrender.com',
         changeOrigin: true,
       },
     },
